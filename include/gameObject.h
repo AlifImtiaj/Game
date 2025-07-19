cf56be3025a84131eac73cbfaabc34aadd64f7dc
Alif Imtiaj <lolalif412@gmail.com>
2025-07-19 21:51:10 +0600

    did something

--- include/gameObject.h
+++ include/gameObject.h
@@ -10,9 +10,18 @@ namespace GameEngine {
 		std::string m_name;
 		sf::Vector2f m_position;
 		sf::Vector2f m_rotation;
+
+		static int gameObjCount;
 		
 	public:
+		GameObject();
+		GameObject(std::string name, sf::Vector2f position = { 0,0 }, sf::Vector2f rotation = { 0,0 });
 
+		virtual void Start();
+		virtual void Update(float deltatime);
+		virtual void Render();
+
+		void Destroy();
 
 	};
 }
